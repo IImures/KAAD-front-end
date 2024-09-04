e51829a9d7fd28e7396aa2acc8b87fe9b8752297
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-09-04 19:56:29 +0200

    Feature: Better time representation

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
 import {PostComponent} from "./post/post.component";
+import {PostDetails} from "../../interfaces/post-details";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
   imports: [
-    PostComponent
+    PostComponent,
+    NgForOf
   ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
 export class BlogComponent {
-
+  postsDetails: PostDetails[] = [
+    // {
+    //   postId: '1',
+    //   author: 'Adrian Dzienkiewicz',
+    //   created_at: '1700000000000',
+    //   content: 'Informue bla bla bla',
+    //   contentLength: 3
+    // },
+    // {
+    //   postId: '2',
+    //   author: 'Adrian Dzienkiewicz',
+    //   created_at: '2024-02-10',
+    //   content: '1725450606473',
+    //   contentLength: 3
+    // },
+    {
+      postId: '3',
+      author: 'Adrian Dzienkiewicz',
+      created_at: '1725460004900',
+      content: 'Informue bla bla bla',
+      contentLength: 3
+    },
+    // {
+    //   postId: '4',
+    //   author: 'Adrian Dzienkiewicz',
+    //   created_at: '1725400686473',
+    //   content: 'Informue bla bla bla',
+    //   contentLength: 3
+    // },
+  ]
 }
--- src/app/pages/blog/post/post.component.ts
+++ src/app/pages/blog/post/post.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {PostDetails} from "../../../interfaces/post-details";
+
+const months = [
+  'stycznia',
+  'lutego',
+  'marca',
+  'kwietnia',
+  'maja',
+  'czerwca',
+  'lipca',
+  'sierpnia',
+  'września',
+  'października',
+  'listopada',
+  'grudnia'
+];
 
 @Component({
   selector: 'app-post',
@@ -7,6 +23,80 @@ import { Component } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+
+
+  ngOnInit(): void {
+    console.log(new Date().getTime());
+  }
+  @Input() post!: PostDetails;
+
+  processTime(created_at: string) {
+    let date = new Date(parseInt(created_at));
+    let diffTime = this.getDiffTime(date);
+
+    if(diffTime.totalHours > 8){
+      return this.getFullTime(date);
+    }else{
+      return this.getShortTime(diffTime);
+    }
+
+
+  }
+
+  private getFullTime(date : Date) :string {
+    const year : string = String(date.getFullYear());
+    const month = months[date.getMonth()];
+    const day = date.getDate() < 10 ? '0' + String(date.getDate()) : String(date.getDate());
+
+    const hour = date.getHours() < 10 ? '0' + String(date.getHours()) : String(date.getHours());
+    const minute = date.getMinutes() < 10 ? '0' + String(date.getMinutes()) : String(date.getMinutes());
+    return day + ' ' + month + ' '+ year + ' ' + hour + ':' + minute;
+  }
+
+  private getShortTime(diffTime: any) : string {
+    if (diffTime.totalHours > 0) {
+      const hours = diffTime.totalHours;
+      if (hours === 1) {
+        return `${hours} godzinę temu`;
+      } else if (hours >= 2 && hours <= 4) {
+        return `${hours} godziny temu`;
+      } else {
+        return `${hours} godzin temu`;
+      }
+    } else {
+      const minutes = diffTime.totalMinutes;
+      if (minutes === 1) {
+        return `${minutes} minuta temu`;
+      } else if (minutes >= 2 && minutes <= 4) {
+        return `${minutes} minuty temu`;
+      } else {
+        return `${minutes} minut temu`;
+      }
+    }
+  }
+
+  private getDiffTime(date: Date) {
+    const now = new Date();
+
+    const milliDiff: number = Math.abs(now.getTime() - date.getTime());
+
+    const totalSeconds = Math.floor(milliDiff / 1000);
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const totalHours = Math.floor(totalMinutes / 60);
+
+    console.log(milliDiff);
+    console.log(totalSeconds);
+    console.log(totalMinutes);
+    console.log(totalHours);
+
+
+    return {
+      milliDiff,
+      totalSeconds,
+      totalMinutes,
+      totalHours,
+    };
+  }
 
 }
