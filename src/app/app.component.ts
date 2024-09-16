cd3d26fedc6ccb5f22f05fedc1b86dd55fc3b4f8
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-09-16 15:03:16 +0200

    Fix: Added check for browser to evade window not found exception

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./pages/header/header.component";
 import {FooterComponent} from "./pages/footer/footer.component";
+import {isPlatformBrowser} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,17 @@ import {FooterComponent} from "./pages/footer/footer.component";
 })
 export class AppComponent implements OnInit{
   title = 'kancelaria Adwokacka';
+  isBrowser: boolean = false;
 
   constructor(
     private router: Router,
+    @Inject(PLATFORM_ID) platformId: Object
   ) {
+    this.isBrowser = isPlatformBrowser(platformId);
   }
 
   ngOnInit(): void {
+    if(!this.isBrowser) {return}
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         window.scrollTo(0, 0);
