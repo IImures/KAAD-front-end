79b9db46ded239d56f3db442e8db6d38777bc653
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-10-06 11:50:19 +0200

    Feature: Added HttpClientProvider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {provideRouter, withInMemoryScrolling} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,9 @@ export const appConfig: ApplicationConfig = {
       routes,
       withInMemoryScrolling()
     ),
-    provideClientHydration()]
+    provideHttpClient(
+      withFetch(),
+     ),
+    provideClientHydration()
+  ]
 };
